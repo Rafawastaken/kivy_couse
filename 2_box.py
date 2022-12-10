e3285837a229d263353caf1f465720819b400e2d
Rafael Pimenta <jrafapimenta@gmail.com>
2022-12-10 03:24:08 +0000

    inherit

---
+++ 2_box.py
@@ -0,0 +1,18 @@
+import kivy
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.lang import Builder
+
+# Import KV File
+Builder.load_file('2_inherit.kv')
+
+class MyLayout(Widget):
+    pass
+
+class BoxLayoutApp(App):
+    def build(self):
+        return MyLayout()
+
+
+if __name__ == '__main__':
+    BoxLayoutApp().run()
